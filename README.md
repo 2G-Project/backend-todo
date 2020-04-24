f22d4e4fc54e70d789d731a50cd3ad7dfd9464c8
ken1286 <ken.ridenour4@gmail.com>
2020-04-24 07:19:55 -0700

    Cleaned up seed data

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 3. `npx knex seed:run` to run seed mock data (optional).
 
 4. `npm run dev` to run the server locally.
+
+Login and Register work to create a new user
+username: `user1` password: `12345` is a mock user with some premade todos.
--- data/seeds/002_todos.js
+++ data/seeds/002_todos.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex) {
   return knex('todos').insert([
-    { user_id: 1, text: 'this is todotext1', is_complete: 1 },
-    { user_id: 1, text: 'this is todotext2' },
-    { user_id: 1, text: 'this is todotext3' },
+    { user_id: 1, text: 'This is todotext1', is_complete: 1 },
+    { user_id: 1, text: 'This is todotext2' },
+    { user_id: 1, text: 'This is todotext3' },
   ]);
 };
